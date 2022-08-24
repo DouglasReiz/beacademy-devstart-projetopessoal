814a633f3e0dc1ba418bf1d47221588cbf31064d
Douglas Alves <douglasreisalves02@gmail.com>
2022-08-24 08:49:40 -0300

    ajustes na view da index

--- my-clients/resources/views/index.blade.php
+++ my-clients/resources/views/index.blade.php
@@ -7,11 +7,11 @@
     <h1 class="text-center my-4">Olá Bem vindo</h1>
 
     <div class="col-lg-6 mx-auto my-4 text-center">
-        <p class="lead mb-4">Vamos direto ao ponto, veja seus clientes.</p>
+        <p class="lead mb-4">Escolha uma das opções abaixo</p>
 
         <a href="{{ route('clients.showEach', Auth::user()->id) }}" class="btn btn-outline-dark">Meus Clientes {{Auth::user()->clients->count()}}</a>
 
-        <a href="{{ route('clients.create') }}" class="btn btn-outline-dark">Novo cliente</a>
+        <a href="{{ route('clients.create') }}" class="btn btn-outline-success">Novo cliente</a>
     </div>
 
 </div>
